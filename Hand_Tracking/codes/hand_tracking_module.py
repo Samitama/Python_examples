0d6cb3f167639090a7a1f79070980ef845c66710
Samitama <90217942+Samitama@users.noreply.github.com>
2022-09-13 00:56:58 +0300

    python_examples

--- Hand_Tracking/codes/hand_tracking_module.py
+++ Hand_Tracking/codes/hand_tracking_module.py
@@ -1,16 +1,17 @@
+from sre_constants import SUCCESS
 import cv2
 import mediapipe as mp
 import time
 
-
 class handDetector:
-    def __init__(self,mode = False, maxHands = 2, detectionCon = 0.5,trackCon = 0.5):
+    def __init__(self, mode = False, maxHands = 2,modelComplex=1, detectionCon = 0.5,trackCon = 0.5):
         self.mode = mode
         self.maxHands = maxHands
+        self.modelComplex = modelComplex
         self.detectionCon = detectionCon
         self.trackCon = trackCon
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode,self.maxHands,
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.modelComplex,
                                         self.detectionCon,self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils    
     def findHands(self,img,draw = True):
@@ -19,29 +20,34 @@ class handDetector:
         if self.results.multi_hand_landmarks :
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmark(img,handLms,self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img,handLms,self.mpHands.HAND_CONNECTIONS)
         return img
-
-     #for id,lm in enumerate(handLms.landmark):
-      #              h, w, c = img.shape
-       #             cx, cy = int(lm.x*w), int(lm.y*h)
-        #            #print(id, cx, cy)
-         #           #if id == 4:
-          #          cv2.circle(img,(cx, cy), 15, (255, 0, 255), cv2.FILLED)
+    def findPosition(self,img,handNo=0,draw=True):
+            lmlist = []
+            if self.results.multi_hand_landmarks:
+                Hand=self.results.multi_hand_landmarks[handNo]
+                for id,lm in enumerate(Hand.landmark):
+                        h, w, c = img.shape
+                        cx, cy = int(lm.x*w), int(lm.y*h)
+                        lmlist.append([id,cx,cy])
+                        if draw:
+                            cv2.circle(img,(cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            return lmlist
 def main():
-        ptime = 0
-        ctime = 0
         cap = cv2.VideoCapture(0)
         Detector = handDetector()
+        ptime = 0
+        ctime = 0
         while True:
-            success, img = cap.read()
+            SUCCESS, img = cap.read()
             img = Detector.findHands(img)
+            lmlist= Detector.findPosition(img)
             ctime = time.time()
             fps = 1/(ctime-ptime)
             ptime = ctime
             cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
             cv2.imshow('sami',img)
-            if cv2.waitKey(30) & 0xFF == ord('esc'):
+            if cv2.waitKey(30) & 0xFF == 27:
                 break
 if __name__ == "__main__":
     main()
